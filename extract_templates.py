ac885abfaef43028f51eaddf343259a60874603f
Dylan Payne <11635831+DylanPayne@users.noreply.github.com>
2023-08-29 15:14:11 -0600

    fixed struct, added .env

---
+++ extract_templates.py
@@ -0,0 +1,8 @@
+import os, logging
+import pandas as pd
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from sqlalchemy import create_engine, text
+from datetime import datetime
+
+
